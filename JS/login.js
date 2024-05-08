cf8454d0d9e79728acc50fad9e6c2343b5ae597d
Stanley Joel N <n.stanley@zarttech.com>
2024-05-08 18:23:38 +0000

    added functionality to our calculator using js

---
+++ JS/login.js
@@ -0,0 +1,6 @@
+function login (){
+    const username =document.getElementById('username').value;
+    const password =document.getElementById('password').value;
+
+    console.log('data: ',username,password)
+}
